bd9030d180439b05659437a06de9b5919ea7ee67
Yoav Natan <yoavnatan.yn@gmail.com>
2025-06-23 20:51:56 +0300

    make an interactive render func

--- js/book.controller.js
+++ js/book.controller.js
@@ -4,3 +4,20 @@ function onInit() {
     render()
 }
 
+
+function render() {
+
+    const elBooksTable = document.querySelector('.main tbody')
+    const books = getBooks()
+
+    var strHTML = books.map(book => `
+            <tr>
+                <td>${book.title}</td>
+                <td>${book.price}</td>
+                <td><span>Read</span><span>Update</span><span>Delete</span></td>
+            </tr>`)
+    elBooksTable.innerHTML = strHTML.join('')
+}
+
+
+
--- js/book.service.js
+++ js/book.service.js
@@ -9,4 +9,3 @@ var gBooks = [
 function getBooks() {
     return gBooks
 }
-
