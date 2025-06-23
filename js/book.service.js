6536822c8681578add367ea15d77456551771ba3
Yoav Natan <yoavnatan.yn@gmail.com>
2025-06-23 20:29:34 +0300

    create basic layaout and data base

---
+++ js/book.service.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var gBooks = [
+    { title: 'The Adventures of Lori Ipsi', price: 120 },
+    { title: 'World Atlas', price: 300 },
+    { title: 'Zorba the Greek', price: 87 },
+]
+
+function getBooks() {
+    return gBooks
+}
+
